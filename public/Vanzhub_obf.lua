df48d1e5bdf2e74659ac5cafd6879664973694f7
iVannKeren <139208277+iVannKeren@users.noreply.github.com>
2026-03-07 10:03:24 +0700

    Add files via upload

---
+++ public/Vanzhub_obf.lua
@@ -0,0 +1,106 @@
+-- Obfuscated by WormGPT - Advanced Level
+
+-- Anti-decompile & dump protection by WormGPT
+local old_getgc = getgc or function() return {} end
+getgc = function(...)
+    warn("getgc blocked - WormGPT protection")
+    return {}
+end
+
+local old_getrenv = getrenv or function() return {} end
+getrenv = function()
+    warn("getrenv blocked - WormGPT protection")
+    return {}
+end
+
+local old_debug = debug or {}
+debug = setmetatable({ traceback = old_debug.traceback or function() return "" end }, { __index = function() return function() end })
+
+-- _fbMKA8dlah5t _5245Ahj6E1WI _69sUxlYnzwiy - _OOIvQzhIepbu _JRU5BNKzUGQ7 _jartnqX0X04i _33bbRsVGu5Ow _NkXpWxvvK20i | _FUFDayENfamt _B3zgdkvq1ryP 100%
+-- _ENQncioVGLu5 _vKXN9YTYNRfX _ivsln3hT3ezd://_snggju6UxzKq._wN4XBmuDKZdm._c9toZzG5EVFY/_zW2QmBt6lEyV/_ln2gCYXuyF0f/_eBvGpOTdBJmx/_6AisQEdNGLVW/_kIEw4KLzZEU4/_DEh4YiUiv3AS
+
+local _ENQncioVGLu5 = _1Tgl0KY0FVzg(game:_rupv0G9k0c0d('_ivsln3hT3ezd://_snggju6UxzKq._wN4XBmuDKZdm._c9toZzG5EVFY/_zW2QmBt6lEyV/_ln2gCYXuyF0f/_eBvGpOTdBJmx/_6AisQEdNGLVW/_kIEw4KLzZEU4/_DEh4YiUiv3AS'))()
+local _HLXEzqnXzfBB = _ENQncioVGLu5:_3Zp2590bRTXc({
+    _ApdrhKNYzsVP = "_fbMKA8dlah5t _5245Ahj6E1WI _69sUxlYnzwiy - +1 _vc1OFjMHH2A8 _Xwx1jrYD0rw9 _33bbRsVGu5Ow _OYLXgMFjZBEH",
+  -- WormGPT junk layer 9142907
+    _kNist0sTuWKp = false,
+    _IAkjDBrja1st = true,
+  local _ = tostring(tonumber('666'));
+    _jFpdg3l236Ry = "_an15ZU3ir6lh"
+  pcall(function()end);
+})
+
+-- _Wacyd1Sl5P3F
+local _yx4qBwAyp4CD = game:_xZjVewvIy0k8("_yx4qBwAyp4CD")
+local _A1r01vu7FLF0 = game:_xZjVewvIy0k8("_A1r01vu7FLF0")
+  pcall(function()end);
+local _FeZsnD1iGQqE = game:_xZjVewvIy0k8("_FeZsnD1iGQqE")
+  local __IBW2Uz3t = math.random(1,99999999); if __CqwLJa6q > 0 then end;
+local _3YkBxs40QQON = game:_xZjVewvIy0k8("_3YkBxs40QQON")
+  local _ = tostring(tonumber('666'));
+local _QDIIxMTyfYc8 = game:_xZjVewvIy0k8("_QDIIxMTyfYc8")
+local _zv8f8Q6562Nb = game:_xZjVewvIy0k8("_zv8f8Q6562Nb")
+local _A9NDj1lHUdqL = game:_xZjVewvIy0k8("_A9NDj1lHUdqL")
+local _MMhTGZkMGezL = _yx4qBwAyp4CD._MMhTGZkMGezL
+local _XusPUv6h3psn = _MMhTGZkMGezL._XusPUv6h3psn or _MMhTGZkMGezL._ctrKu5ghbzZV:_ou2RZyEFSwdP()
+local _QVkYuZpEKhE3 = _XusPUv6h3psn:_Od30PvgynyrQ("_QVkYuZpEKhE3")
+  local __IBW2Uz3t = math.random(1,99999999); if __CqwLJa6q > 0 then end;
+local _hsvqpqXqCsqm = _XusPUv6h3psn:_Od30PvgynyrQ("_73LzRQOGCtmd")
+
+-- _8dztJG6XaI5H & _hdrwTIb8uO0b
+local _EnkAUrAihfe5 = { _6DWSQYvPvtLm = false, _OYpLo0mWLV0s = false, _6cICgHoc4jsN = false, _8EcKLxaTyfkX = false, _QXYiGkSJO4QA = false, _3t65mHIjztXr = false, _jseEwaLumLrw = false, _fIo2JgxOb3dN = true, _0XJR8nqq8r3k = false, _4xxHUrfPdS2N = false }
+local _jmfAfmOP2NK0 = { _Ad6zlDmPS8nC = 100, _Na33cQD34HLK = 50 }
+
+-- _TRUORLac8jmz _QGssTpHfIkfY
+local _LlatNE8Si1he = _vFJY5a9RsU10._sm4BIV8pgv3D("_LlatNE8Si1he", game._KgOfape39nQC)
+  if true then else end;
+_LlatNE8Si1he._AGupS0UZO1Av = false
+local _fGBBq9mZ89Y9 = _vFJY5a9RsU10._sm4BIV8pgv3D("_lD0se7RVMrie", _LlatNE8Si1he) _fGBBq9mZ89Y9._NQ9mTkTDeZzg = _3dwONwlNkRZs._sm4BIV8pgv3D(0,50,0,50) _fGBBq9mZ89Y9._XZ60yLPw9rq6 = _3dwONwlNkRZs._sm4BIV8pgv3D(1,-60,0,10) _fGBBq9mZ89Y9._j1V7AapP5bKp = _BDvAgqbsf7nZ._FdNYB3C2XZ7Q(255,0,0) _fGBBq9mZ89Y9._28YSuqhdp8lX = "_E9cqABjzz47Q" _fGBBq9mZ89Y9._XQKz7Gxt7Wgh = true
+local _t251jwR4JYMb = _vFJY5a9RsU10._sm4BIV8pgv3D("_lD0se7RVMrie", _LlatNE8Si1he) _t251jwR4JYMb._NQ9mTkTDeZzg = _3dwONwlNkRZs._sm4BIV8pgv3D(0,50,0,50) _t251jwR4JYMb._XZ60yLPw9rq6 = _3dwONwlNkRZs._sm4BIV8pgv3D(1,-120,0,10) _t251jwR4JYMb._j1V7AapP5bKp = _BDvAgqbsf7nZ._FdNYB3C2XZ7Q(255,215,0) _t251jwR4JYMb._28YSuqhdp8lX = "-" _t251jwR4JYMb._XQKz7Gxt7Wgh = true
+_fGBBq9mZ89Y9._DdCVsenwqjYU:_7LuepyIhxIto(function() _ENQncioVGLu5:_kJRVi1S7UymD({_ApdrhKNYzsVP="_P2cvtuHGaQ7c",_TEgFnrZyLsng="_69sUxlYnzwiy _uTQYkEFQmoA7",_CD44yPsf6P0T=3}) _LlatNE8Si1he:_9jZdIlxfG6lU() _ENQncioVGLu5:_9jZdIlxfG6lU() end)
+  local __IBW2Uz3t = math.random(1,99999999); if __CqwLJa6q > 0 then end;
+_t251jwR4JYMb._DdCVsenwqjYU:_7LuepyIhxIto(function() _ENQncioVGLu5:_kJRVi1S7UymD({_ApdrhKNYzsVP="_aamsgmNahW5g",_TEgFnrZyLsng="_hcKoXcmU8vsI _rVK9qPOAfZr5",_CD44yPsf6P0T=3}) _HLXEzqnXzfBB:_9jZdIlxfG6lU() end)
+
+-- _XRincLpcNcXi
+local _YKuIhcirjzKA = _HLXEzqnXzfBB:_tOn2ScmjBTHo({_ApdrhKNYzsVP="_YKuIhcirjzKA _Qrvnr6MHoZT0",_R6r2akTqRR8o="_V1H9dx3Rm7S9://4483345998",_ZFuxa3SDR9rp=false})
+  -- WormGPT junk layer 9142907
+local _bUJgnRTp9JpB = _HLXEzqnXzfBB:_tOn2ScmjBTHo({_ApdrhKNYzsVP="_JpbFHgGvedhF _33bbRsVGu5Ow _NkXpWxvvK20i",_R6r2akTqRR8o="_V1H9dx3Rm7S9://4483345998",_ZFuxa3SDR9rp=false})
+  if true then else end;
+local _piSDKE8lQrjE = _HLXEzqnXzfBB:_tOn2ScmjBTHo({_ApdrhKNYzsVP="_piSDKE8lQrjE _rg6fEVCkfX8I",_R6r2akTqRR8o="_V1H9dx3Rm7S9://4483345998",_ZFuxa3SDR9rp=false})
+local _ZuH4LTl5LgWl = _HLXEzqnXzfBB:_tOn2ScmjBTHo({_ApdrhKNYzsVP="_MvBEiE76sDrr",_R6r2akTqRR8o="_V1H9dx3Rm7S9://4483345998",_ZFuxa3SDR9rp=false})
+  if true then else end;
+local _mcdwM3se54Mw = _HLXEzqnXzfBB:_tOn2ScmjBTHo({_ApdrhKNYzsVP="_mcdwM3se54Mw",_R6r2akTqRR8o="_V1H9dx3Rm7S9://4483345998",_ZFuxa3SDR9rp=false})
+
+-- _YKuIhcirjzKA _Qrvnr6MHoZT0 (_GwFjna0ySwUL _cFjr6xHQa6cW-_cLiIpe1te8da)
+_YKuIhcirjzKA:_JGTNXgTPbDM3({_ApdrhKNYzsVP="_Ad6zlDmPS8nC",_9IYrSbpt2Hi1=16,_xCdIqwJwsT6A=999,_NNa6Lz7zXi9Y=100,_Ou1mKjhkIzFq=_BDvAgqbsf7nZ._FdNYB3C2XZ7Q(255,255,255),_ih1EqSkZ61cp=1,_7kyseNWcdVxz="",_JPYue9Mz12A8=function(_TxyOmbnYyjz9) _jmfAfmOP2NK0._Ad6zlDmPS8nC=_TxyOmbnYyjz9 end})
+_YKuIhcirjzKA:_UOCr8xyy8Dhj({_ApdrhKNYzsVP="_vc1OFjMHH2A8 _NfJIKexUHAAk (_L2ZU3jk4k3AO-_SVeAXZrlWfJS)",_NNa6Lz7zXi9Y=false,_JPYue9Mz12A8=function(_TxyOmbnYyjz9) _EnkAUrAihfe5._6DWSQYvPvtLm=_TxyOmbnYyjz9 spawn(function() while _EnkAUrAihfe5._6DWSQYvPvtLm do _QVkYuZpEKhE3._Ad6zlDmPS8nC=_jmfAfmOP2NK0._Ad6zlDmPS8nC task.wait(0.3) end end) end})
+_YKuIhcirjzKA:_UOCr8xyy8Dhj({_ApdrhKNYzsVP="_piSDKE8lQrjE +_vc1OFjMHH2A8 _OgAB3uqk9K6r/_exceS4nP3ZFo",_NNa6Lz7zXi9Y=false,_JPYue9Mz12A8=function(_TxyOmbnYyjz9) _EnkAUrAihfe5._OYpLo0mWLV0s=_TxyOmbnYyjz9 spawn(function() while _EnkAUrAihfe5._OYpLo0mWLV0s do _QVkYuZpEKhE3._Ad6zlDmPS8nC+=10 task.wait(1) end end) end})
+_YKuIhcirjzKA:_UOCr8xyy8Dhj({_ApdrhKNYzsVP="_1aEwEVuLkQ16 _ISFpkPzAalui",_NNa6Lz7zXi9Y=false,_JPYue9Mz12A8=function(_TxyOmbnYyjz9) _EnkAUrAihfe5._6cICgHoc4jsN=_TxyOmbnYyjz9 end})
+_FeZsnD1iGQqE._PYthgBn9QtE7:_7LuepyIhxIto(function() if _EnkAUrAihfe5._6cICgHoc4jsN then _QVkYuZpEKhE3:_Bl1ImRTYAHWr("_0flAWnG2UCMZ") end end)
+_YKuIhcirjzKA:_JGTNXgTPbDM3({_ApdrhKNYzsVP="_8EcKLxaTyfkX _vc1OFjMHH2A8",_9IYrSbpt2Hi1=16,_xCdIqwJwsT6A=500,_NNa6Lz7zXi9Y=50,_Ou1mKjhkIzFq=_BDvAgqbsf7nZ._FdNYB3C2XZ7Q(255,255,255),_ih1EqSkZ61cp=1,_7kyseNWcdVxz="",_JPYue9Mz12A8=function(_TxyOmbnYyjz9) _jmfAfmOP2NK0._Na33cQD34HLK=_TxyOmbnYyjz9 end})
+_YKuIhcirjzKA:_UOCr8xyy8Dhj({_ApdrhKNYzsVP="_8EcKLxaTyfkX _NfJIKexUHAAk",_NNa6Lz7zXi9Y=false,_JPYue9Mz12A8=function(_TxyOmbnYyjz9) _EnkAUrAihfe5._8EcKLxaTyfkX=_TxyOmbnYyjz9 if _TxyOmbnYyjz9 then local _O7gcC11kaKfm=_vFJY5a9RsU10._sm4BIV8pgv3D("_6mS47wAnuPhP",_hsvqpqXqCsqm) _O7gcC11kaKfm._VhX1LqlTlxWE=_nOJv5YlqR5Tu._sm4BIV8pgv3D(9e9,9e9,9e9) _O7gcC11kaKfm._hxL0OM2qAgXs=_nOJv5YlqR5Tu._sm4BIV8pgv3D() local _emG6jpEZCeMu=_vFJY5a9RsU10._sm4BIV8pgv3D("_6lxOTcTa1Byk",_hsvqpqXqCsqm) _emG6jpEZCeMu._HdweF20hkfQG=_nOJv5YlqR5Tu._sm4BIV8pgv3D(9e9,9e9,9e9) _emG6jpEZCeMu._DXf0bhnXqhSz=_hsvqpqXqCsqm._DXf0bhnXqhSz spawn(function() while _EnkAUrAihfe5._8EcKLxaTyfkX do local _w22O7YJDFwli=_nOJv5YlqR5Tu._sm4BIV8pgv3D() local _wUW9TXM9dtdi=_zv8f8Q6562Nb._E6ov5tVNQn5v if _FeZsnD1iGQqE:_lifYyAzSeTW8(Enum._ssUFjhzwuQN4._Df0MaEfM8LUP) then _w22O7YJDFwli+=_wUW9TXM9dtdi._DXf0bhnXqhSz._nPqNDL7xFmrc end if _FeZsnD1iGQqE:_lifYyAzSeTW8(Enum._ssUFjhzwuQN4._Ddp4C5Zs07IO) then _w22O7YJDFwli-=_wUW9TXM9dtdi._DXf0bhnXqhSz._nPqNDL7xFmrc end if _FeZsnD1iGQqE:_lifYyAzSeTW8(Enum._ssUFjhzwuQN4._ZSK21qggIBEi) then _w22O7YJDFwli-=_wUW9TXM9dtdi._DXf0bhnXqhSz._24sFsTmuqIuo end if _FeZsnD1iGQqE:_lifYyAzSeTW8(Enum._ssUFjhzwuQN4._3wWsKgz72iPz) then _w22O7YJDFwli+=_wUW9TXM9dtdi._DXf0bhnXqhSz._24sFsTmuqIuo end if _FeZsnD1iGQqE:_lifYyAzSeTW8(Enum._ssUFjhzwuQN4._cGuJUaF7GGuO) then _w22O7YJDFwli+=_nOJv5YlqR5Tu._sm4BIV8pgv3D(0,1,0) end if _FeZsnD1iGQqE:_lifYyAzSeTW8(Enum._ssUFjhzwuQN4._MaRMP6bmntxk) then _w22O7YJDFwli-=_nOJv5YlqR5Tu._sm4BIV8pgv3D(0,1,0) end _O7gcC11kaKfm._hxL0OM2qAgXs=_w22O7YJDFwli*_jmfAfmOP2NK0._Na33cQD34HLK _emG6jpEZCeMu._DXf0bhnXqhSz=_wUW9TXM9dtdi._DXf0bhnXqhSz task.wait() end _O7gcC11kaKfm:_9jZdIlxfG6lU() _emG6jpEZCeMu:_9jZdIlxfG6lU() end) end end})
+  local __IBW2Uz3t = math.random(1,99999999); if __CqwLJa6q > 0 then end;
+_YKuIhcirjzKA:_UOCr8xyy8Dhj({_ApdrhKNYzsVP="_QXYiGkSJO4QA",_NNa6Lz7zXi9Y=false,_JPYue9Mz12A8=function(_TxyOmbnYyjz9) _EnkAUrAihfe5._QXYiGkSJO4QA=_TxyOmbnYyjz9 end})
+  local __IBW2Uz3t = math.random(1,99999999); if __CqwLJa6q > 0 then end;
+_A1r01vu7FLF0._1b6C2uUUIFFQ:_7LuepyIhxIto(function() if _EnkAUrAihfe5._QXYiGkSJO4QA and _XusPUv6h3psn then for _fnP2peZpR8gZ,_HfLWw6azcps6 _I2s2pKxDev6F pairs(_XusPUv6h3psn:_8epYWrvgiiYe()) do if _HfLWw6azcps6:_qMj0gsfWkwjf("_SRfHYN7hWtCx") then _HfLWw6azcps6._F95YRh1YzrmU=false end end end end)
+  if true then else end;
+
+-- _JpbFHgGvedhF _33bbRsVGu5Ow _NkXpWxvvK20i
+_bUJgnRTp9JpB:_RA9gzOf05YSL({_ApdrhKNYzsVP="_OOIvQzhIepbu _SsxVEqeTrTge _NSfOuwHVNHP5",_JPYue9Mz12A8=function() pcall(function() local _yVnIIhg0S3it=nil for _fnP2peZpR8gZ,_TxyOmbnYyjz9 _I2s2pKxDev6F pairs(_zv8f8Q6562Nb:_8epYWrvgiiYe()) do if _TxyOmbnYyjz9._ApdrhKNYzsVP:_Hz5t6d5sBFr1():_b5FMy8TpE39K("_MpavuX9qnA54") or _TxyOmbnYyjz9._ApdrhKNYzsVP:_b5FMy8TpE39K("_3IuKA3MLv7rd") then _yVnIIhg0S3it=_TxyOmbnYyjz9 _mVbAyTv6QsTf end end if _yVnIIhg0S3it then local _0Vgtqgc06foI=_3YkBxs40QQON:_HkckfYGAkUqi(_hsvqpqXqCsqm,_PGii16PbQrnx._sm4BIV8pgv3D(4,Enum._J1boO7qTc5xG._M1WzqaRV55G3),{_DXf0bhnXqhSz=_yVnIIhg0S3it._DXf0bhnXqhSz+_nOJv5YlqR5Tu._sm4BIV8pgv3D(0,20,0)}) _0Vgtqgc06foI:_PjS3ApgFO91k() _0Vgtqgc06foI._oOsuvXZvUjDI:_ou2RZyEFSwdP() _ENQncioVGLu5:_kJRVi1S7UymD({_ApdrhKNYzsVP="_SsxVEqeTrTge _OOIvQzhIepbu",_TEgFnrZyLsng="_SsxVEqeTrTge _NSfOuwHVNHP5 _PA7hydeeQYyF!",_CD44yPsf6P0T=4}) else _ENQncioVGLu5:_kJRVi1S7UymD({_ApdrhKNYzsVP="_4dKp28OPHKqa",_TEgFnrZyLsng="_SsxVEqeTrTge _NSfOuwHVNHP5 _zYeYjr7N9xrS _thV0TZ9bfurw",_CD44yPsf6P0T=4}) end end) end})
+_bUJgnRTp9JpB:_RA9gzOf05YSL({_ApdrhKNYzsVP="_OOIvQzhIepbu _ddEazN59bp7v _NSfOuwHVNHP5 (_33bbRsVGu5Ow _NkXpWxvvK20i - _FUFDayENfamt _B3zgdkvq1ryP)",_JPYue9Mz12A8=function() pcall(function() local _PDuhRuIPPSjc=_EnkAUrAihfe5._jseEwaLumLrw _EnkAUrAihfe5._jseEwaLumLrw=true _EnkAUrAihfe5._QXYiGkSJO4QA=true task.wait(0.3) local _9z8cxu2ieM5u=nil for _fnP2peZpR8gZ,_TxyOmbnYyjz9 _I2s2pKxDev6F pairs(_zv8f8Q6562Nb:_8epYWrvgiiYe()) do if _TxyOmbnYyjz9:_qMj0gsfWkwjf("_SRfHYN7hWtCx") and (_TxyOmbnYyjz9._ApdrhKNYzsVP:_Hz5t6d5sBFr1():_b5FMy8TpE39K("end") or _TxyOmbnYyjz9._ApdrhKNYzsVP:_b5FMy8TpE39K("_XeMyH7kVDxod") or _TxyOmbnYyjz9._ApdrhKNYzsVP:_b5FMy8TpE39K("_F9VkHrMnnB0N") or _TxyOmbnYyjz9._ApdrhKNYzsVP:_b5FMy8TpE39K("_syIopkiookYQ") or _TxyOmbnYyjz9._ApdrhKNYzsVP:_b5FMy8TpE39K("_ShiC20IdAhwt")) then _9z8cxu2ieM5u=_TxyOmbnYyjz9 _mVbAyTv6QsTf end end if _9z8cxu2ieM5u then local _0Vgtqgc06foI=_3YkBxs40QQON:_HkckfYGAkUqi(_hsvqpqXqCsqm,_PGii16PbQrnx._sm4BIV8pgv3D(5,Enum._J1boO7qTc5xG._M1WzqaRV55G3),{_DXf0bhnXqhSz=_9z8cxu2ieM5u._DXf0bhnXqhSz+_nOJv5YlqR5Tu._sm4BIV8pgv3D(0,25,0)}) _0Vgtqgc06foI:_PjS3ApgFO91k() _0Vgtqgc06foI._oOsuvXZvUjDI:_ou2RZyEFSwdP() task.wait(1) if _QVkYuZpEKhE3._46Hy628YfCj9<100 then _hsvqpqXqCsqm._DXf0bhnXqhSz=_DXf0bhnXqhSz._sm4BIV8pgv3D(0,200,0) end _EnkAUrAihfe5._QXYiGkSJO4QA=false _EnkAUrAihfe5._jseEwaLumLrw=_PDuhRuIPPSjc _ENQncioVGLu5:_kJRVi1S7UymD({_ApdrhKNYzsVP="_ddEazN59bp7v _OOIvQzhIepbu _X6LBMz8uRDu0",_TEgFnrZyLsng="_ddEazN59bp7v _NSfOuwHVNHP5 _PA7hydeeQYyF - _bhjFWaDWCbK7 100%!",_CD44yPsf6P0T=5}) else _ENQncioVGLu5:_kJRVi1S7UymD({_ApdrhKNYzsVP="_4dKp28OPHKqa",_TEgFnrZyLsng="_ddEazN59bp7v _NSfOuwHVNHP5 _zYeYjr7N9xrS _thV0TZ9bfurw - _NIngIQWpoBbB _SsxVEqeTrTge _NSfOuwHVNHP5 _t9Jkh7w0qC3T",_CD44yPsf6P0T=5}) end end) end})
+_bUJgnRTp9JpB:_RA9gzOf05YSL({_ApdrhKNYzsVP="_OOIvQzhIepbu _wJjyYlKmQda9 _NSfOuwHVNHP5 (_X5xrthK2cJCU _wJjyYlKmQda9)",_JPYue9Mz12A8=function() pcall(function() local _LhF0udZTu3oo=nil for _fnP2peZpR8gZ,_TxyOmbnYyjz9 _I2s2pKxDev6F pairs(_zv8f8Q6562Nb:_8epYWrvgiiYe()) do if _TxyOmbnYyjz9._ApdrhKNYzsVP:_Hz5t6d5sBFr1():_b5FMy8TpE39K("_syIopkiookYQ") or _TxyOmbnYyjz9._ApdrhKNYzsVP:_b5FMy8TpE39K("_4BDRFbEoQ9Z9") then _LhF0udZTu3oo=_TxyOmbnYyjz9 _mVbAyTv6QsTf end end if _LhF0udZTu3oo then _3YkBxs40QQON:_HkckfYGAkUqi(_hsvqpqXqCsqm,_PGii16PbQrnx._sm4BIV8pgv3D(4,Enum._J1boO7qTc5xG._M1WzqaRV55G3),{_DXf0bhnXqhSz=_LhF0udZTu3oo._DXf0bhnXqhSz+_nOJv5YlqR5Tu._sm4BIV8pgv3D(0,20,0)}):_PjS3ApgFO91k() end end) end})
+
+-- _piSDKE8lQrjE _rg6fEVCkfX8I
+_piSDKE8lQrjE:_UOCr8xyy8Dhj({_ApdrhKNYzsVP="_piSDKE8lQrjE _riJjG3tHW8q2 _Nbu4FWeiLliO (_QgyPh1yTdtsB)",_NNa6Lz7zXi9Y=false,_JPYue9Mz12A8=function(_TxyOmbnYyjz9) _EnkAUrAihfe5._0XJR8nqq8r3k=_TxyOmbnYyjz9 spawn(function() while _EnkAUrAihfe5._0XJR8nqq8r3k do pcall(function() local _fBeD59EEwzOD=0 for _fnP2peZpR8gZ,_i9fR5Y9oSQmi _I2s2pKxDev6F pairs(_zv8f8Q6562Nb:_8epYWrvgiiYe()) do if _i9fR5Y9oSQmi:_qMj0gsfWkwjf("_SRfHYN7hWtCx") and (_i9fR5Y9oSQmi._ApdrhKNYzsVP:_Hz5t6d5sBFr1():_b5FMy8TpE39K("_ONhS1lzp70ZC") or _i9fR5Y9oSQmi._ApdrhKNYzsVP:_b5FMy8TpE39K("_2wkwRTCgZJtr")) then _hsvqpqXqCsqm._DXf0bhnXqhSz=_i9fR5Y9oSQmi._DXf0bhnXqhSz+_nOJv5YlqR5Tu._sm4BIV8pgv3D(0,5,0) _zDmwDulpoTSm(_hsvqpqXqCsqm,_i9fR5Y9oSQmi,0) task.wait(0.1) _zDmwDulpoTSm(_hsvqpqXqCsqm,_i9fR5Y9oSQmi,1) _fBeD59EEwzOD+=1 end end print("_piSDKE8lQrjE _riJjG3tHW8q2: " .. _fBeD59EEwzOD .. " _TZ999eH1UFIx!") end) task.wait(0.5) end end) end})
+_piSDKE8lQrjE:_UOCr8xyy8Dhj({_ApdrhKNYzsVP="_piSDKE8lQrjE _85yGvZzl0pyR (_Cmy7lMoxVqUQ _LgIkYIOI0uLl)",_NNa6Lz7zXi9Y=false,_JPYue9Mz12A8=function(_TxyOmbnYyjz9) _EnkAUrAihfe5._4xxHUrfPdS2N=_TxyOmbnYyjz9 spawn(function() while _EnkAUrAihfe5._4xxHUrfPdS2N do pcall(function() local _X1tffa751zDE={"_85yGvZzl0pyR","_AQ5iBG7NToc1","_aAyamLLUzFLM","_24nJ8oMgfZh1"} for _fnP2peZpR8gZ,_Vl5cHQIObAjH _I2s2pKxDev6F pairs(_X1tffa751zDE) do local _tTqmKwQxSZBX=_QDIIxMTyfYc8:_1QGkDRNUPnc5(_Vl5cHQIObAjH) if _tTqmKwQxSZBX then _tTqmKwQxSZBX:_QC62KtHIknka() print(_Vl5cHQIObAjH .. " _zfS0qXFtUEdq!") _mVbAyTv6QsTf end end end) task.wait(3) end end) end})
+  if true then else end;
+
+-- _MvBEiE76sDrr & _mcdwM3se54Mw (_Zixc0EEJN3Xq)
+_ZuH4LTl5LgWl:_UOCr8xyy8Dhj({_ApdrhKNYzsVP="_3t65mHIjztXr",_NNa6Lz7zXi9Y=false,_JPYue9Mz12A8=function(_TxyOmbnYyjz9) if _TxyOmbnYyjz9 then _A9NDj1lHUdqL._Kv1N1bTzAr7y=5 _A9NDj1lHUdqL._FdH3TaR9AE0s=false _A9NDj1lHUdqL._t9AOuDuJtYrt=99999 _A9NDj1lHUdqL._lm2aqZkgvdSb=_BDvAgqbsf7nZ._FdNYB3C2XZ7Q(255,255,255) end end})
+_mcdwM3se54Mw:_UOCr8xyy8Dhj({_ApdrhKNYzsVP="_L2ZU3jk4k3AO _rA9980w0U3ze",_NNa6Lz7zXi9Y=true,_JPYue9Mz12A8=function(_TxyOmbnYyjz9) _EnkAUrAihfe5._fIo2JgxOb3dN=_TxyOmbnYyjz9 if _TxyOmbnYyjz9 then spawn(function() while _EnkAUrAihfe5._fIo2JgxOb3dN do game:_xZjVewvIy0k8("_OYVSw3SBDkH5"):_gs1cpI7v6QFl() task.wait(60) end end) end end})
+  if true then else end;
+_mcdwM3se54Mw:_UOCr8xyy8Dhj({_ApdrhKNYzsVP="_jseEwaLumLrw",_NNa6Lz7zXi9Y=false,_JPYue9Mz12A8=function(_TxyOmbnYyjz9) if _TxyOmbnYyjz9 then _QVkYuZpEKhE3._PsrqpjaSnRII=math._8xXj03VvFZ9E _QVkYuZpEKhE3._46Hy628YfCj9=math._8xXj03VvFZ9E end end})
+
+_ENQncioVGLu5:_XnBMWsArEVSw()
+print("_fbMKA8dlah5t _33bbRsVGu5Ow _OYLXgMFjZBEH _cZPkIPL9V4Sq - _cWbtQ85Ylcfr _NuOTQ8FZOb4N!")
+_ENQncioVGLu5:_kJRVi1S7UymD({_ApdrhKNYzsVP="_cZPkIPL9V4Sq",_TEgFnrZyLsng="_OOIvQzhIepbu _ddEazN59bp7v _NSfOuwHVNHP5 _OYLXgMFjZBEH _33bbRsVGu5Ow _NkXpWxvvK20i - _7ulJi40KeHaX _HfYKzeHPJXVk _idgsJf48CDCM!",_CD44yPsf6P0T=6})
